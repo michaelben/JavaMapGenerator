eaca411e84588058e86a2368e39b89de2f1fd352
Michael Zhang <michaelsmallbiz99@gmail.com>
2014-11-28 00:12:24 +0800

    adjust param minimum values. remove slider extent to fix value<=max-extent bug.

--- src/com/mapgen/Param.java
+++ src/com/mapgen/Param.java
@@ -23,13 +23,13 @@ public class Param {
 	}
 	
 	public static Param[] params = {
-		new Param("map x-axis length(~1000m)", 10000, 0, 1000, 100),
-		new Param("map y-axis length(~1000m)", 10000, 0, 800, 100),
-		new Param("average street width(>15m)", 100, 0, 15, 1),
+		new Param("map x-axis length(~1000m)", 10000, 100, 1000, 100),
+		new Param("map y-axis length(~1000m)", 10000, 100, 800, 100),
+		new Param("average street width(>15m)", 100, 1, 15, 1),
 		new Param("street width divergence(m)", 100, 0, 2, 1),
-		new Param("average building height(m)", 100, 0, 7, 1),
+		new Param("average building height(m)", 100, 1, 7, 1),
 		new Param("building height divergence(m)", 100, 0, 6, 1),
-		new Param("building facade length(m)", 100, 0, 30, 1)
+		new Param("building facade length(m)", 100, 1, 30, 1)
 	};
 	
 	public static int getMapWidth() {
